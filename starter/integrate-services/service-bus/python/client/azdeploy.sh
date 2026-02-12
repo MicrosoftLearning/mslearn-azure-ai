552dcfb558b416ed56c1746161845a9fe7fc328e
jeffkoms <jeffko@microsoft.com>
2026-02-12 14:23:20 -0800

    fixed scripts

--- starter/integrate-services/service-bus/python/client/azdeploy.sh
+++ starter/integrate-services/service-bus/python/client/azdeploy.sh
@@ -2,11 +2,8 @@
 
 # Change the values of these variables as needed
 
-# rg="<your-resource-group-name>"  # Resource Group name
-# location="<your-azure-region>"   # Azure region for the resources
-
-rg="rg-exercises"           # Resource Group name
-location="eastus2"          # Azure region for the resources
+rg="<your-resource-group-name>"  # Resource Group name
+location="<your-azure-region>"   # Azure region for the resources
 
 # ============================================================================
 # DON'T CHANGE ANYTHING BELOW THIS LINE.
