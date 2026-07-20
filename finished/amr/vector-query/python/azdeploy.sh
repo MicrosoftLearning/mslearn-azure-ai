08399c6b873be47a7719d46ff7dc1d3b3463e715
jeffkoms <jeffko@microsoft.com>
2026-07-20 14:48:21 -0700

    udpated

--- finished/amr/vector-query/python/azdeploy.sh
+++ finished/amr/vector-query/python/azdeploy.sh
@@ -6,7 +6,7 @@
 # location="<your-azure-region>"   # Azure region for the resources
 
 rg="rg-exercises" # Resource Group name
-location="westus2" # Azure region for the resources
+location="canadacentral" # Azure region for the resources
 
 # ============================================================================
 # DON'T CHANGE ANYTHING BELOW THIS LINE.
